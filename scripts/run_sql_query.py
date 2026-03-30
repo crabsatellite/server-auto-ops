3c8a2d9a5de4835f2e32a9fa18a7067a7f161a84
Alex <islicy@outlook.com>
2026-03-30 20:18:34 +0900

    add run-sql workflow for ad-hoc SQL queries on VPS

---
+++ scripts/run_sql_query.py
@@ -0,0 +1,22 @@
+"""Run an ad-hoc SQL query on VPS. Usage: python run_sql_query.py <database> <query>"""
+import sys
+from ecs_control import get_status
+from deploy_sql import run_sql
+
+if len(sys.argv) < 3:
+    print("Usage: python run_sql_query.py <database> <query>")
+    sys.exit(1)
+
+db = sys.argv[1]
+query = sys.argv[2]
+
+status = get_status()
+if status != 'Running':
+    print(f'VPS is {status}, cannot run query')
+    sys.exit(1)
+
+print(f'Database: {db}')
+print(f'Query: {query}')
+print('---')
+result = run_sql(db, query=query, timeout=60)
+print(result)
